75d7b0f412960e73cece43e7fbf17962ce0997fe
Administrator <wltngus200@gmail.com>
2024-03-15 12:33:25 +0900

    Green

---
+++ src/main/java/day2/CompOperator.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class CompOperator {
+    public static void main(String[] args){
+        //정수형 데이터 타입인 int형
+        //불린형, TF를 구분하는 데이터 타입
+        boolean b1=true;
+        boolean b2=false;
+
+        boolean r1=10>9;//10이 9보다 크냐
+        System.out.println(r1);
+
+        r1=10<9;//10이 9보다 작냐
+        System.out.println(r1);
+
+        r1=10>=9;
+        System.out.println(r1);
+
+        int a=2;
+        boolean r2=a<=2;
+        System.out.println(r2);
+
+        
+    }
+}

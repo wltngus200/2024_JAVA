2e8c50954553282fabb0408b66d6d562de1a0cfc
Administrator <wltngus200@gmail.com>
2024-03-18 09:32:58 +0900

    Green

--- src/main/java/practice/practice01.java
+++ src/main/java/practice/practice01.java
@@ -1,4 +1,24 @@
 package practice;
 
+import java.util.Scanner;
+
 public class practice01 {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int[] ban=new int[5];
+        for(int i=0;i<5;i++){
+            System.out.print((i+1)+"번의 성적을 입력하세요.");
+            ban[i]=input.nextInt();
+        }
+        int sum=0;
+        int people=0;
+        for(int j=0;j<5;j++){
+            sum+=ban[j];
+            people++;
+        }
+        System.out.println("점수의 총 합은 "+sum+"점입니다.");
+        System.out.println("점수의 평균은 "+(double)(sum/people)+"점입니다.");
+        System.out.println(people);//위에서 j나 i를 재사용할 수 없어서 설정했음
+
+    }
 }

83cb2612e9c536260606de7d3f35bdc4406247da
Administrator <wltngus200@gmail.com>
2024-03-18 15:44:23 +0900

    Green

---
+++ src/main/java/day3/MissionForLoopGugudan.java
@@ -0,0 +1,8 @@
+package day3;
+
+public class MissionForLoopGugudan {
+    public static void main(String[] args){
+        int dan=(int)(Math.random()*8)+2;
+        System.out.println(dan);
+    }
+}

673a67e76f9ea73ea245aa99f4555a64e3d7406b
Administrator <wltngus200@gmail.com>
2024-03-18 16:50:02 +0900

    Green

---
+++ src/main/java/day3/MissionMultipleForLoop.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class MissionMultipleForLoop {
+    public static void main(String[] args){
+        for(int i=0;i<10;i++) {
+            int star=(int)(Math.random()*5)+2;
+            System.out.println(star);
+        }
+    }
+}

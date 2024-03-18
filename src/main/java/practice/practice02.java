ed0370486999445241608cbed7d58675651796b9
Administrator <wltngus200@gmail.com>
2024-03-18 18:10:16 +0900

    Green

--- src/main/java/practice/practice02.java
+++ src/main/java/practice/practice02.java
@@ -10,15 +10,17 @@ public class practice02 {
         //직각삼각형
         */
 
-        int num=10;
-        for(int i=0;i<num;i++) {
-            for (int j=1;j<num-i;j++) {
+
+        for(int i=0;i<10;i++) { //아래 것을 10줄까지 반복
+
+            for (int j=1;j<10-i;j++) { //-i가 있어 둘째줄이면 1개, 셋째줄이면 2개가 빠짐
                 System.out.print(" "); //공백
             }
-            for(int k=0;k<i*2+1;k++){
+
+            for(int k=0;k<i*2+1;k++){ //*2가 있어 둘째줄이면 2개+1개, 셋째줄이면 4+1개
                 System.out.print("*");// *
             }
-                System.out.println();
+                System.out.println(); //한 줄을 채우면 엔터를 친다
         } //이등변 삼각형 모양
     }
 }

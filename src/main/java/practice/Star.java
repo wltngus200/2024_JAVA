fc0f5bcd9a7df708eb41c487bb46d36aa40fe8cb
Administrator <wltngus200@gmail.com>
2024-03-19 12:45:15 +0900

    Green

---
+++ src/main/java/practice/Star.java
@@ -0,0 +1,22 @@
+package practice;
+public class Star {
+
+    public static void main(String[] args){
+        for(int i=0;i<5;i++) {//(초기화, 조건식, 증감식) 여기선 i=4까지
+            for (int z = 0; z <= i; z++) { //i가 4까지기 때문에 4<=4
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        /*
+
+        *       0 - 1
+        **      1 - 2
+        ***     2 - 3
+        ****    3 - 4
+        *****   4 - 5
+        로 나와야 됨
+
+         */
+    }
+}

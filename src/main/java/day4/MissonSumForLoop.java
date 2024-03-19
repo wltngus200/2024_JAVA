dc7f2996163139533b2582cacc4c74f784ae7cc4
Administrator <wltngus200@gmail.com>
2024-03-19 13:10:20 +0900

    Green

--- src/main/java/day4/MissonSumForLoop.java
+++ src/main/java/day4/MissonSumForLoop.java
@@ -5,7 +5,7 @@ public class MissonSumForLoop {
         //1~10까지 더한 값 출력
         int sum=0;
         for(int i=1;i<=10;i++){//첨엔 0~10으로 했는데 1이 맞네
-            //sum+=i;
+            //sum+=i; 복합식 가능하넹 안 알려주셔서 안 쓰셨던 듯
             sum=sum+i;
             //i대신 sum을 쓰면 이상한 값 나와=>디버깅 해서 체크
             //for문을 돌면 계속 새거가 되기 때문

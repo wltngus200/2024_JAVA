43efcb5ca7802e0372e4078cd4dc6b6da079f786
Administrator <wltngus200@gmail.com>
2024-03-19 14:02:54 +0900

    Green

---
+++ src/main/java/day4/MissionSumWhileLoop.java
@@ -0,0 +1,24 @@
+package day4;
+
+import java.util.Scanner;
+
+public class MissionSumWhileLoop {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        int sum=0;
+        int input=0;
+        //입력한 숫자를 모두 더한 값 출력
+        while(true){//
+            System.out.print("숫자를 입력해주세요.(종료:0)");
+            input=scan.nextInt();
+            if (input == 0) {
+                break;
+        } else {
+                sum=sum+input;
+            }
+        System.out.println("종료");
+        System.out.println(sum);
+
+    }
+}
